e23258f9ca9a590123b06995ba50e0da6f609e96
Kaidong Hu <hukaidonghkd@gmail.com>
2025-09-17 14:26:21 -0400

    feat(config): enable ESS module and enhance email configuration
    - Enable ESS (Emacs Speaks Statistics) module for statistical computing
    - Refactor mu4e email configuration to support multiple contexts
    - Add separate contexts for Home (Gmail) and Work (Rutgers) accounts
    - Integrate davmail service startup with after-init-hook
    - Configure appropriate folder mappings for each email context
    
    This enhances the editor's capabilities for data analysis and provides
    better email workflow management with context-aware folder handling.

--- mail-config.el
+++ mail-config.el
@@ -1,13 +1,43 @@
 ;;; mail-config.el --- Email configuration (mu4e) -*- lexical-binding: t; -*-
 
-(setq user-mail-address "hukaidonghkd@gmail.com")
-
 (after! mu4e
-  (setq mu4e-drafts-folder "/Drafts")
-  (setq mu4e-sent-folder "/Sent")
-  (setq mu4e-refile-folder "/Archive")
+  (setq mu4e-contexts
+        (list
+         ;; Work account
+         (make-mu4e-context
+          :name "Home"
+          :match-func
+          (lambda (msg)
+            (when msg
+              (string-prefix-p "/Home" (mu4e-message-field msg :maildir))))
+          :vars '((user-mail-address . "hukaidonghkd@gmail.com")
+                  (user-full-name    . "Kaidong Hu")
+                  (mu4e-drafts-folder  . "/Home/[Gmail]/Drafts")
+                  (mu4e-sent-folder  . "/Home/[Gmail]/Sent Mail")
+                  (mu4e-refile-folder  . "/Home/[Gmail]/All Mail")
+                  (mu4e-trash-folder . "/Home/[Gmail]/Trash")
+                  ))
+         ;; Work account
+         (make-mu4e-context
+          :name "Work"
+          :match-func
+          (lambda (msg)
+            (when msg
+              (string-prefix-p "/Work" (mu4e-message-field msg :maildir))))
+          :vars '((user-mail-address . "kaidong.hu@rutgers.edu")
+                  (user-full-name    . "Kaidong Hu")
+                  (mu4e-drafts-folder  . "/Work/Drafts")
+                  (mu4e-sent-folder  . "/Work/Sent")
+                  (mu4e-refile-folder  . "/Work/Archive")
+                  (mu4e-trash-folder . "/Work/Trash")
+                  ))
+         ))
   (setq mu4e-get-mail-command "mbsync --all")
   (setq mu4e-update-interval 300)
   (setq mu4e-headers-auto-update t))
+
+;; Start davmail using Doom's add-hook! macro
+(add-hook! 'after-init-hook
+  (start-process "davmail" nil "systemctl" "--user" "start" "davmail"))
 
 ;;; mail-config.el ends here
