ffc94d06a4bf0d5b74a90db9b99d63a56c97fe7b
Kaidong Hu <hukaidonghkd@gmail.com>
2025-12-07 13:24:18 -0500

    feat(config): enable rainbow-delimiters for programming modes

--- general.el
+++ general.el
@@ -5,4 +5,5 @@
 (setq user-full-name "Kaidong Hu"
       user-mail-address "hukaidonghkd@gmail.com")
 
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 ;;; general.el ends here
--- packages.el
+++ packages.el
@@ -49,3 +49,5 @@
 ;; (unpin! t)
 
 (package! eca :recipe (:host github :repo "editor-code-assistant/eca-emacs" :files ("*.el")))
+
+(package! rainbow-delimiters)
